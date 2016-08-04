3d8e08be617061ee9f6a715ac7464ac524bda1b8
Romel Pérez <ronelprhone@gmail.com>
2016-08-03 23:38:02 -0500

    chore: demo server

--- demo/server.js
+++ demo/server.js
@@ -1,11 +1,13 @@
-var express = require('express');
+const express = require('express');
 
-var port = process.env.PORT || 7777;
-var app = express();
+const port = process.env.PORT || 7777;
+const app = express();
 
 app.use(express.static(__dirname.replace('/demo', '')));
 
+app.get('/', (req, res) => res.redirect('/demo'));
+
 app.listen(port, function(err) {
-    if (err) throw err;
-    console.log('Server running at http://127.0.0.1:'+ port);
+  if (err) throw err;
+  console.log('Server running at http://127.0.0.1:'+ port);
 });
